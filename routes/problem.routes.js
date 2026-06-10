fb78b7f95820edbfafa19c364dda3a173b6311cd
Ankit Kumar Pandey <763281ap@gmail.com>
2026-06-10 11:56:32 +0530

    feat: add route of delete problem

--- routes/problem.routes.js
+++ routes/problem.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createProblem, getAllProblem,getProblemBySlug,updateProblem } from "../controllers/problem.controllers.js";
+import { createProblem, getAllProblem,getProblemBySlug,updateProblem,deleteProblem } from "../controllers/problem.controllers.js";
 import { authenticate } from "../middleware/auth.middleware.js";
 import { authorize } from "../middleware/auth.middleware.js";
 
@@ -9,5 +9,6 @@ router.post("/create", authenticate, authorize("admin"), createProblem);
 router.get("/all", authenticate, authorize("admin"), getAllProblem);
 router.get("/:slug", getProblemBySlug);
 router.put("/:id", authenticate, authorize("admin"), updateProblem);
+router.delete("/:id", authenticate, authorize("admin"), deleteProblem);
 
 export default router;
