58a95331f1cedd39ca2078d53baad58ab77b8cad
Ankit Kumar Pandey <763281ap@gmail.com>
2026-06-10 19:39:34 +0530

    feat: add update testcase route

--- routes/test.routes.js
+++ routes/test.routes.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { createTestCase,getTestCases } from "../controllers/test.controllers.js"
+import { createTestCase,getTestCases,updateTestCase } from "../controllers/test.controllers.js"
 import { authenticate,authorize } from "../middleware/auth.middleware.js"
 
 const router=express.Router();
 
 router.post("/problem/:problemId",authenticate,authorize("admin"),createTestCase)
 router.get("/problem/:problemId",authenticate,authorize("admin"),getTestCases)
+router.put("/:id",authenticate,authorize("admin"),updateTestCase)
 
 export default router;
