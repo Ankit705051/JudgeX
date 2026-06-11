d248f13db5059dde6d08a5e5b60a350153e99c51
Ankit Kumar Pandey <763281ap@gmail.com>
2026-06-11 13:21:19 +0530

    feat:add getting submission endpoint

--- routes/submission.routes.js
+++ routes/submission.routes.js
@@ -1,10 +1,13 @@
 import express from "express";
 
-import {submitCode} from "../controllers/submission.controllers.js";
-import { authenticate,authorize } from "../middleware/auth.middleware.js";
+import {submitCode,getSubmissionById,getMySubmissions} from "../controllers/submission.controllers.js";
+import { authenticate } from "../middleware/auth.middleware.js";
 
 const router=express.Router();
 
 router.post("/submit",authenticate,submitCode);
+router.get("/my-submissions",authenticate,getMySubmissions);
+router.get("/:id",authenticate,getSubmissionById);
+
 
 export default router;
