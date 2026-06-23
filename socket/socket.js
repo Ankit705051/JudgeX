8aa642bf3632ef6325392570d083260940ae838b
Ankit Kumar Pandey <763281ap@gmail.com>
2026-06-23 21:43:47 +0530

    feat:add websocket

---
+++ socket/socket.js
@@ -0,0 +1,24 @@
+import {Server} from "socket.io";
+let io;
+
+export const initSocket=(server)=>{
+    io=new Server(server,{
+        cors:{
+            origin: "http://localhost:5173",
+            credentials: true,
+        }
+    });
+    io.on("connection",(socket)=>{
+        console.log("client connected",socket.id);
+        socket.on("join",(userId)=>{
+            socket.join(userId);
+        });
+        socket.on("disconnect",()=>{
+            console.log("client Disconnected");
+        });
+     });
+     return io;
+
+}
+
+export const getIo=()=>io;
