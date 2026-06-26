c06a08b590c47a9438ed60b74c7df918a354a140
Ankit Kumar Pandey <763281ap@gmail.com>
2026-06-26 16:14:36 +0530

    feat:add cosmuers for leaderboard update

---
+++ consumers/leaderboard.consumer.js
@@ -0,0 +1,31 @@
+import Redis from "ioredis";
+
+
+const subscriber = new Redis();
+const redis = new Redis();
+
+async function startLeaderboardConsumer() {
+    await subscriber.subscribe("leaderboard-updates");
+      
+    console.log("leaderborad consumer started ...");
+
+    subscriber.on("message", async (channel, message) => {
+        try{
+            const data=JSON.parse(message);
+            const{
+                contestId,
+                userId,
+                score,
+            }=data;
+            const leaderboradKey=`leaderboard:${contestId}`;
+            await redis.zadd(leaderboradKey,score, userId);
+
+            console.log(`Updated leaderboard for contest ${contestId} with user ${userId} and score ${score}`);
+            
+        }catch(error){
+            console.error("Error processing leaderboard update:", error);
+        }
+    });
+}
+
+startLeaderboardConsumer();
