99f690d476ba8d0fa92154dcf43a8cf631e3a53d
Ankit Kumar Pandey <763281ap@gmail.com>
2026-07-01 19:42:58 +0530

    fix:redis connection

--- config/redis.js
+++ config/redis.js
@@ -1,15 +1,19 @@
 import IORedis from "ioredis";
 
-export const redis = new IORedis({
-    host: process.env.REDIS_HOST || "localhost",
-    port: process.env.REDIS_PORT || 6379,
-    maxRetriesPerRequest: null,
-});
+export const redis = process.env.REDIS_URL
+  ? new IORedis(process.env.REDIS_URL, {
+      maxRetriesPerRequest: null,
+    })
+  : new IORedis({
+      host: process.env.REDIS_HOST || "localhost",
+      port: Number(process.env.REDIS_PORT) || 6379,
+      maxRetriesPerRequest: null,
+    });
 
 redis.on("connect", () => {
-    console.log("Redis client connected");
+  console.log("Redis client connected");
 });
 
 redis.on("error", (err) => {
-    console.error("Redis client error:", err);
+  console.error("Redis client error:", err);
 });
