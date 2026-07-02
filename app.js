0ee2a39875c645f1204546a7bebd7bc64c720f3f
Ankit Kumar Pandey <763281ap@gmail.com>
2026-07-02 09:04:32 +0530

    feat:add frontend endpoint

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app=express();
 
 
 app.use(cors({
-    origin:["http://localhost:5173"],
+    origin:["http://localhost:5173", "https://sprightly-kataifi-1a0126.netlify.app"],
     credentials:true,
 }));
 
