666e955d1d96d6e3b30f0363ff22d9ac55545242
Ankit Kumar Pandey <763281ap@gmail.com>
2026-07-14 14:50:17 +0530

    fix:cros error

--- app.js
+++ app.js
@@ -13,17 +13,8 @@ import discussionRouter from "./routes/discussion.routes.js";
 const app=express();
 
 
-const whitelist = ["http://localhost:5173", process.env.FRONTEND_URL].filter(Boolean);
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || whitelist.includes(origin)) {
-            callback(null, true);
-        } else if (process.env.NODE_ENV === "production") {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
+    origin: "*",
     credentials: true,
 }));
 
